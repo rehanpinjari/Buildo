d7dcd756254579fe73884e871c77b27a6765fc09
Pinjari Rehan <122555029+rehanpinjari@users.noreply.github.com>
2023-07-31 09:39:14 +0500

    Update Topnav.js

--- src/Components/Topnav.js
+++ src/Components/Topnav.js
@@ -4,67 +4,67 @@ import Logo from "../Buildo.png";
 export default function Topnav() {
   return (
     <div>
-      // <div>
-      //   <nav className="navbar navbar-dark bg-dark fixed-top">
-      //     <div className="container-fluid">
-      //       <img className="logo-2" src={Logo} alt="" width={50} />
-      //       <a className="navbar-brand" href="#">
-      //         Buildo
-      //       </a>
-      //       <button
-      //         className="navbar-toggler"
-      //         type="button"
-      //         data-bs-toggle="offcanvas"
-      //         data-bs-target="#offcanvasDarkNavbar"
-      //         aria-controls="offcanvasDarkNavbar"
-      //       >
-      //         <span className="navbar-toggler-icon" />
-      //       </button>
-      //       <div
-      //         className="offcanvas offcanvas-end text-bg-dark"
-      //         tabIndex={-1}
-      //         id="offcanvasDarkNavbar"
-      //         aria-labelledby="offcanvasDarkNavbarLabel"
-      //       >
-      //         <div className="offcanvas-header">
-      //           <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-      //             Buildo | Website Builder
-      //           </h5>
-      //           <button
-      //             type="button"
-      //             className="btn-close btn-close-white"
-      //             data-bs-dismiss="offcanvas"
-      //             aria-label="Close"
-      //           />
-      //         </div>
-      //         <div className="offcanvas-body">
-      //           <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-      //             <li className="nav-item">
-      //               <a className="nav-link active" aria-current="page" href="#">
-      //                 Home
-      //               </a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">
-      //                 Resources
-      //               </a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">
-      //                 Community
-      //               </a>
-      //             </li>
-      //             <li className="nav-item">
-      //               <a className="nav-link" href="#">
-      //                 Help
-      //               </a>
-      //             </li>
-      //           </ul>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </nav>
-      // </div>
+      <div>
+        <nav className="navbar navbar-dark bg-dark fixed-top">
+          <div className="container-fluid">
+            <img className="logo-2" src={Logo} alt="" width={50} />
+            <a className="navbar-brand" href="#">
+              Buildo
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasDarkNavbar"
+              aria-controls="offcanvasDarkNavbar"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="offcanvas offcanvas-end text-bg-dark"
+              tabIndex={-1}
+              id="offcanvasDarkNavbar"
+              aria-labelledby="offcanvasDarkNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                  Buildo | Website Builder
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close btn-close-white"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Resources
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Community
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Help
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
